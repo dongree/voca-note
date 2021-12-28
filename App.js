b23f9e2d41a0c90b787cb2c28c562c7dea36ec38
dongree <dongwoo0307@naver.com>
2021-12-28 22:08:15 +0900

    basic setting and navigation

--- App.js
+++ App.js
@@ -1,20 +1,108 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.home}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Management')}
+      >
+        <Text style={styles.buttonText}>카드 관리</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.navigate('Quiz')}
+      >
+        <Text style={styles.buttonText}>랜덤 퀴즈</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
+function ManagementScreen({ navigation }) {
+  return (
+    <View style={styles.management}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.goBack()}
+      >
+        <Text style={styles.buttonText}>Go back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function QuizScreen({ navigation }) {
+  return (
+    <View style={styles.quiz}>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => navigation.goBack()}
+      >
+        <Text style={styles.buttonText}>Go back</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'My home' }}
+        />
+        <Stack.Screen name="Management" component={ManagementScreen} />
+        <Stack.Screen name="Quiz" component={QuizScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+
+    // <StatusBar style="auto" />
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  home: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ff8e47',
+  },
+
+  management: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff8e47',
+  },
+
+  quiz: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ff8e47',
+  },
+
+  button: {
+    backgroundColor: '#f5b342',
+    marginBottom: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+
+  buttonText: {
+    fontSize: 20,
   },
 });
